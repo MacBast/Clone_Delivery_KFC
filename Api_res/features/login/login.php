143768cb12cfbd3478e6a8941c00c97eff4fb2e7
MacBast <danielbastidas459@gmail.com>
2022-03-24 12:45:54 -0500

    modificando carpeta

---
+++ Api_res/features/login/login.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once(dirname(__DIR__)."../../db/db_config.php");
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    //var_dump($data);
+    $data = json_decode(file_get_contents('php://input'), true);    
+    $identification= $data['identification'];
+    $db =new DBConfig();
+    $dbConnection = $db->connect();
+    $query = "SELECT * FROM users WHERE identification='$identification'";
+    $users = $dbConnection->query($query)->fetchall(PDO::FETCH_ASSOC);
+    header('Content-Type: application/json');
+    echo (json_encode($users[0]));
+}else{
+    echo "No hay conexion";
+}
+
+
+
