58471d5fa790747af402ed496be8ab9adaaee94d
mafe2812 <mamolina812@gmail.com>
2022-04-07 21:20:56 -0500

    clase 07 04 22

--- Api_res/features/list-users/list-users.php
+++ Api_res/features/list-users/list-users.php
@@ -2,7 +2,16 @@
 require_once(dirname(__DIR__)."../../db/db_config.php");
 $db = new DBConfig();
 $dbConnection = $db->connect();
-$query = "SELECT * FROM users";
-$users = $dbConnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+$users= "";
+if(isset($_GET['user'])){
+    $search= $_GET['search'];
+    $query = "SELECT * FROM users WHERE 'dintification='$search'";
+    $users = $dbConnection->query($query)->fetchAll(PDO::FETCH_ASSOC)[0];
+
+}else{
+    $query = "SELECT * FROM users";
+    $users = $dbConnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+}
 header('Content-Type: application/json');
 echo json_encode($users);
